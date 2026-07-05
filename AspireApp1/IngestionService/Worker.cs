eae1292b2513cc876eccceb08d396988196da17c
prushorth <maniprush3@gmail.com>
2026-07-04 22:00:13 -0700

    Read icm from disc

--- AspireApp1/IngestionService/Worker.cs
+++ AspireApp1/IngestionService/Worker.cs
@@ -4,12 +4,19 @@ public class Worker(ILogger<Worker> logger) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        var directoryInfo = new DirectoryInfo("icm_incident_dataset");
+
         while (!stoppingToken.IsCancellationRequested)
         {
+            var filesToProcess = directoryInfo.EnumerateFiles("*.md");
+
             if (logger.IsEnabled(LogLevel.Information))
             {
                 logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                logger.LogInformation("Files to process: {files}",
+                    string.Join(", ", filesToProcess.Select(f => f.Name)));
             }
+
             await Task.Delay(1000, stoppingToken);
         }
     }
