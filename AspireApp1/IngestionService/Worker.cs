663013b32b1a9a66cce6db435f515088487929b5
prushorth <maniprush3@gmail.com>
2026-07-05 16:05:54 -0700

    Add icm reader

--- AspireApp1/IngestionService/Worker.cs
+++ AspireApp1/IngestionService/Worker.cs
@@ -1,5 +1,5 @@
 using Microsoft.Extensions.DataIngestion;
-
+using System.Diagnostics;
 namespace IngestionService;
 
 public class Worker(ILoggerFactory loggerFactory, ILogger<Worker> logger) : BackgroundService
@@ -21,7 +21,7 @@ public class Worker(ILoggerFactory loggerFactory, ILogger<Worker> logger) : Back
                     string.Join(", ", filesToProcess.Select(f => f.Name)));
             }
 
-            var pipeline = new IngestionPipeline<string>(reader: null, chunker: null, writer: null, loggerFactory: loggerFactory);
+            var pipeline = new IngestionPipeline<string>(reader: new IcmReader(), chunker: null, writer: null, loggerFactory: loggerFactory);
 
             await foreach (var result in pipeline.ProcessAsync(filesToProcess, stoppingToken))
             {
@@ -34,6 +34,18 @@ public class Worker(ILoggerFactory loggerFactory, ILogger<Worker> logger) : Back
             await File.AppendAllLinesAsync(trackingFilePath, filesToProcess.Select(f => f.Name), stoppingToken);
 
             await Task.Delay(1000, stoppingToken);
+        }
+    }
+
+    public class IcmReader: IngestionDocumentReader 
+    {
+
+        private readonly MarkdownReader _markdownReader = new();
+        public override Task<IngestionDocument> ReadAsync(Stream source, string identifier, string mediaType,   CancellationToken cancellationToken)
+        {
+           Debug.WriteLine(identifier);
+           Debug.WriteLine(mediaType);
+           return _markdownReader.ReadAsync(source, identifier, mediaType, cancellationToken);
         }
     }
 }
